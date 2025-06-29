80cb0555826478a0db8415c53567b499b6925e45
Hum-jr <bennyangiye202@gmail.com>
2025-06-29 11:01:01 +0300

    mob updates

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Main : Node
 {
+	
+	[Export]
+	public PackedScene MobScene { get; set; }
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +14,18 @@ public partial class Main : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void _on_mob_timer_timeout()
+	{
+		Mob mob = MobScene.Instantiate<Mob>();
+
+		var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+		
+		Vector3 playerPosition = GetNode<Player>("Player").Position;
+		mob.InitializeMob(mobSpawnLocation.Position, playerPosition);
+		
+		AddChild(mob);
+		
 	}
 }
